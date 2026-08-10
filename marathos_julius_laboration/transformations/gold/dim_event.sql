3fe5483ec55ad57967e82098029ae76a4ca4e5bb
Skoga02 <julius.faltskoglund@gmail.com>
2026-08-10 18:40:34 +0000

    added: dim_event

---
+++ marathos_julius_laboration/transformations/gold/dim_event.sql
@@ -0,0 +1,13 @@
+CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.dim_event
+    COMMENT "Event dimension" AS
+SELECT DISTINCT
+    event_id,
+    event_name,
+    event_type,
+    event_distance_length,
+    event_distance_km,
+    event_country,
+    event_date,
+    year_of_event,
+    event_number_of_finishers
+FROM marathos.silver.cleaned_marathos_2;
