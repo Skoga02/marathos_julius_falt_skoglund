1458f8569e7ebb046272475518b871f6a43e5daf
Skoga02 <julius.faltskoglund@gmail.com>
2026-08-11 18:42:54 +0000

    fix: deleted athlete_club (bug)

--- marathos_julius_laboration/transformations/gold/dim_athlete.sql
+++ marathos_julius_laboration/transformations/gold/dim_athlete.sql
@@ -1,11 +1,10 @@
 CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.dim_athlete
-  COMMENT "Athlete dimension" AS
+  COMMENT "Dim table - gold layer" AS
 SELECT DISTINCT
   athlete_id,
   athlete_country,
   athlete_year_of_birth,
   athlete_gender,
-  athlete_age_category,
-  athlete_club
+  athlete_age_category
 FROM
   marathos.silver.cleaned_marathos_2;
