2a0b1e60db05fd484817964889910a7ee772fe03
Skoga02 <julius.faltskoglund@gmail.com>
2026-08-11 06:48:17 +0000

    Fix: structure

--- marathos_julius_laboration/transformations/gold/dim_athlete.sql
+++ marathos_julius_laboration/transformations/gold/dim_athlete.sql
@@ -1,10 +1,11 @@
 CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.dim_athlete
-    COMMENT "Athlete dimension" AS
+  COMMENT "Athlete dimension" AS
 SELECT DISTINCT
-    athlete_id,
-    athlete_country,
-    athlete_year_of_birth,
-    athlete_gender,
-    athlete_age_category,
-    athlete_club
-FROM marathos.silver.cleaned_marathos_2;
+  athlete_id,
+  athlete_country,
+  athlete_year_of_birth,
+  athlete_gender,
+  athlete_age_category,
+  athlete_club
+FROM
+  marathos.silver.cleaned_marathos_2;
--- marathos_julius_laboration/transformations/gold/dim_event.sql
+++ marathos_julius_laboration/transformations/gold/dim_event.sql
@@ -1,13 +1,14 @@
 CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.dim_event
-    COMMENT "Event dimension" AS
+  COMMENT "Event dimension" AS
 SELECT DISTINCT
-    event_id,
-    event_name,
-    event_type,
-    event_distance_length,
-    event_distance_km,
-    event_country,
-    event_date,
-    year_of_event,
-    event_number_of_finishers
-FROM marathos.silver.cleaned_marathos_2;
+  event_id,
+  event_name,
+  event_type,
+  event_distance_length,
+  event_distance_km,
+  event_country,
+  event_date,
+  year_of_event,
+  event_number_of_finishers
+FROM
+  marathos.silver.cleaned_marathos_2;
