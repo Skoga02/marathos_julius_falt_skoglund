5b366500583b5c7e00face18277794393fa2d2e3
Skoga02 <julius.faltskoglund@gmail.com>
2026-08-11 18:44:58 +0000

    fix: event_id bug

--- marathos_julius_laboration/transformations/gold/dim_event.sql
+++ marathos_julius_laboration/transformations/gold/dim_event.sql
@@ -1,14 +1,16 @@
 CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.dim_event
   COMMENT "Event dimension" AS
-SELECT DISTINCT
+SELECT
   event_id,
-  event_name,
-  event_type,
-  event_distance_length,
-  event_distance_km,
-  event_country,
-  event_date,
-  year_of_event,
-  event_number_of_finishers
+  FIRST(event_name) AS event_name,
+  FIRST(event_type) AS event_type,
+  FIRST(event_distance_length) AS event_distance_length,
+  FIRST(event_distance_km) AS event_distance_km,
+  FIRST(event_country) AS event_country,
+  FIRST(event_date) AS event_date,
+  FIRST(year_of_event) AS year_of_event,
+  SUM(event_number_of_finishers) AS event_number_of_finishers
 FROM
-  marathos.silver.cleaned_marathos_2;
+  marathos.silver.cleaned_marathos_2
+GROUP BY
+  event_id;
