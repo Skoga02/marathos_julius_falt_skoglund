45b202acec6b42f612e94f70cdd4ceea8e9f4783
Skoga02 <julius.faltskoglund@gmail.com>
2026-08-11 03:55:02 +0000

    added: dim_athlete

---
+++ marathos_julius_laboration/transformations/gold/dim_athlete.sql
@@ -0,0 +1,10 @@
+CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.dim_athlete
+    COMMENT "Athlete dimension" AS
+SELECT DISTINCT
+    athlete_id,
+    athlete_country,
+    athlete_year_of_birth,
+    athlete_gender,
+    athlete_age_category,
+    athlete_club
+FROM marathos.silver.cleaned_marathos_2;
